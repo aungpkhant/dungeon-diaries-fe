c688f6ce1d38e920de1a373ea16b596365238c05
aungpkhant <derrick.aungkhant@gmail.com>
2021-12-18 19:59:32 +0630

    Top level comments + button props

--- src/features/comments/components/CommentForm.tsx
+++ src/features/comments/components/CommentForm.tsx
@@ -1,3 +1,55 @@
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
+import { TextAreaField } from '@/components/Form/TextAreaField';
+import { Button } from '@/components/Elements';
+import { useCreateComment } from '../api/createComment';
+import { useParams } from 'react-router';
+
+const schema = z.object({
+  content: z.string().min(1, 'Content is required').max(1000, 'Maximum 1000 characters'),
+});
+// parentCommentId: z.number().nullable(),
+
 export const CommentForm = () => {
-  return <div>Comment form</div>;
+  const { mutate, isLoading } = useCreateComment();
+  const { postId } = useParams();
+
+  const methods = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      parentCommentId: null,
+      content: '',
+    },
+  });
+
+  const newCommentSubmit = (data: any) => {
+    if (postId) {
+      mutate(
+        { postId, data },
+        {
+          // Improvement: scroll to new comment after successful post
+          onSuccess: () => {
+            methods.reset();
+          },
+        }
+      );
+    }
+  };
+
+  return (
+    <form className="px-4 sm:px-0" onSubmit={methods.handleSubmit(newCommentSubmit)}>
+      <TextAreaField
+        label="Comment"
+        {...methods.register('content')}
+        error={methods.formState.errors['content']}
+      />
+      <div className="flex  justify-end pt-2">
+        <Button variant="primary" type="submit" isLoading={isLoading}>
+          Publish
+        </Button>
+      </div>
+    </form>
+  );
 };
--- src/features/comments/components/CommentsList.tsx
+++ src/features/comments/components/CommentsList.tsx
@@ -18,7 +18,7 @@ export const CommentsList = () => {
   if (!data?.length) {
     return (
       <div className="bg-white rounded-lg shadow-sm w-full h-48 flex justify-center items-center text-gray-500 font-medium">
-        -- Much Empty --
+        -- No Comments --
       </div>
     );
   }
