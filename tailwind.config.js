51a2b715a4860f6bc9f3e3bab603a4de0f8791a8
aungpkhant <derrick.aungkhant@gmail.com>
2022-02-06 03:23:38 +1100

    Tweak fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Source Sans Pro', 'sans-serif'],
+      heading: ['Munro', 'sans-serif'],
+    },
   },
   variants: {
     extend: {
